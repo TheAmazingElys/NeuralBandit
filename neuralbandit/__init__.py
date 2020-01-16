dcc00a745cada2e9b3158f875c235bd6c96ad850
rallesiardo <robin.allesiardo@gmail.com>
2020-01-16 19:09:35 +0100

    Initialization of poetry

--- neuralbandit/__init__.py
+++ neuralbandit/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.1.0'
+
 from .data import get_cov_dataset
 from .game import ContextualBanditGame
 from .algorithm import NeuralBandit
---
+++ tests/test_neuralbandit.py
@@ -0,0 +1,5 @@
+from neuralbandit import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
